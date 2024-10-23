51b9d78437661a813636fbaadb711be39ad435a2
Martine Rusås <martine.rusas@gmail.com>
2024-10-23 06:58:59 +0200

    Update readme og bakekalkulator

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Bakekalkulator
 Kalkulator for å ta inn oppskrifter, som så omdanner oppskriften til ønsket antall porsjoner
+
+
+## Fremtidig funksjonalitet
+- Øke/redusere grovhet
+- Kakefunksjonalitet: øke/redusere steketid, -temp og anbefalt kakeformstørrelse ved endret antall porsjoner
--- bakekalkulator.py
+++ bakekalkulator.py
@@ -1,5 +1,5 @@
 class Bakekalkulator:
-    """ En klasse som tar inn antall porsjoner oppskriften gir, ingredienser og ønsket antall porsjoner, og returnerer en ingrediensliste med ønsket antall porsjoner """
+    """ En klasse som tar inn antall porsjoner oppskriften gir, ingredienser og ønsket antall porsjoner, og kan returnere en ingrediensliste med ønsket antall porsjoner """
     def __init__(self, porsjonerInn, ingredienser, onsketAntallPorsjoner):
         self.porsjonerInn = porsjonerInn
         self.ingredienser = ingredienser
@@ -7,3 +7,11 @@ class Bakekalkulator:
 
     def omdanningsfaktor(self):
         return self.onsketAntallPorsjoner / self.porsjonerInn
+    
+    def nyListe(self):
+        nyListeIng = []
+        for ing in self.ingredienser:
+            nyListeIng.append(ing * self.omdanningsfaktor)
+        return nyListeIng
+    
+    
